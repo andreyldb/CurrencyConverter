cb0ab25129d5934f19aacb41395334cc606fe4a7
Andrey <andreydiasbarros@hotmail.com>
2018-03-16 17:48:33 -0300

    e la vamos nos

--- app/components/Container/Container.js
+++ app/components/Container/Container.js
@@ -5,7 +5,6 @@ import { View } from 'react-native';
 import styles from './styles';
 
 const Container = ({ children }) => (
-<<<<<<< HEAD
   <View style={styles.container}>
     {children}
   </View>
@@ -13,15 +12,6 @@ const Container = ({ children }) => (
 
 Container.propTypes = {
   children: PropTypes.any,
-=======
-    <View style={styles.container}>
-        {children}
-    </View>
-);
-
-Container.propTypes = {
-    children: PropTypes.any,
->>>>>>> 6c7a57e2aa5aac67f12b929c6696e8353dbc9d6f
 };
 
 export default Container;
