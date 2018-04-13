9f48f3c82dfd7c31b9aaf884802c63d34185ffc3
Anne Gabrielle <annegabs65@gmail.com>
2018-04-13 17:45:37 -0300

    tamo indo 13.04.2018

--- app/index.js
+++ app/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
-import Home from './screens/Home';
+import CurrencyList from './screens/CurrencyList';
 
-EStyleSheet.build({
-    $primaryBlue: '#4F6D7A',
-    $white: '#FFFFFF',
-    $lightGray: '#F0F0F0',
-    $border: '#979797',
-    $inputText: '#797979',
+EStyleSheet.build ({
+  $primaryBlue: '#4F6D7A',
+  $white: '#FFFFFF',
+  $lightGray: '#F0F0F0',
+  $border: '#E2E2E2',
+  $inputText: '#797979',
+  $darkText: '#343434',
 });
- 
-export default () => <Home />;
 
+export default () => <CurrencyList />;
