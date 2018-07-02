649d1c52c5c51b1b519d5e09971f54c6e32f42c6
Gabriel Augusto <gabriel_abcunha@hotmail.com>
2018-07-02 16:42:31 -0300

    ta pronto professor

--- app/components/List/ListItem.js
+++ app/components/List/ListItem.js
@@ -13,17 +13,26 @@ const ListItem = ({
   visible = true,
   customIcon = null,
   iconBackground,
-}) => (
+}) => {
+  const textStyles = [styles.text];
+  if (iconBackground) {
+    textStyles.push({ color: iconBackground });
+  }
+
+  return (
     <TouchableHighlight onPress={onPress} underlayColor={styles.$underlayColor}>
       <View style={styles.row}>
-        <Text style={styles.text}>{text}</Text>
-        {selected
-          ? <Icon visible={visible} checkmark={checkmark} iconBackground={iconBackground} />
-          : <Icon />}
+        <Text style={textStyles}>{text}</Text>
+        {selected ? (
+          <Icon visible={visible} checkmark={checkmark} iconBackground={iconBackground} />
+        ) : (
+          <Icon />
+        )}
         {customIcon}
       </View>
     </TouchableHighlight>
   );
+};
 
 ListItem.propTypes = {
   text: PropTypes.string,
@@ -36,3 +45,4 @@ ListItem.propTypes = {
 };
 
 export default ListItem;
+
